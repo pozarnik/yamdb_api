ca20462c49e1dd2ae8a16f4db3c2e7d2661ae0a4
pozarnik <pozarnik@tut.by>
2022-03-29 05:04:42 +0300

    Update urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,20 +6,20 @@ from . import views
 app_name = 'api'
 
 router = DefaultRouter()
-router.register(r'v1/users', views.UsersViewSet, basename='users')
-router.register(r'v1/categories', views.CategoryViewSet, basename='categories')
-router.register(r'v1/genres', views.GenreViewSet, basename='genres')
-router.register(r'v1/titles', views.TitleViewSet, basename='titles')
-router.register(r'v1/titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet, basename='reviews')
+router.register(r'users', views.UsersViewSet, basename='users')
+router.register(r'categories', views.CategoryViewSet, basename='categories')
+router.register(r'genres', views.GenreViewSet, basename='genres')
+router.register(r'titles', views.TitleViewSet, basename='titles')
+router.register(r'titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet, basename='reviews')
 router.register(
-    r'v1/titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     views.CommentViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('v1/auth/signup/', views.SignupAPIView.as_view(), name='signup'),
-    path('v1/auth/token/', views.TokenAPIView.as_view(), name='token'),
-    path('v1/users/me/', views.MeAPIView.as_view(), name='current_user'),
+    path('auth/signup/', views.SignupAPIView.as_view(), name='signup'),
+    path('auth/token/', views.TokenAPIView.as_view(), name='token'),
+    path('users/me/', views.MeAPIView.as_view(), name='current_user'),
     path('', include(router.urls), name='api-root'),
 ]
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +28,25 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YaMDb API",
+        default_version='v1',
+        description="Документация для приложения api проекта YaMDb",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
 ]
