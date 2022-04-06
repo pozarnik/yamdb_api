7d9ad5df54cba86a7b46be2b58b8076964a78328
pozarnik <pozarnik@tut.by>
2022-04-06 07:01:35 +0300

    Update

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,10 +6,10 @@ from api import views
 app_name = 'api'
 
 router = DefaultRouter()
-router.register(r'users', views.UsersViewSet, basename='users')
-router.register(r'categories', views.CategoryViewSet, basename='categories')
-router.register(r'genres', views.GenreViewSet, basename='genres')
-router.register(r'titles', views.TitleViewSet, basename='titles')
+router.register('users', views.UsersViewSet, basename='users')
+router.register('categories', views.CategoryViewSet, basename='categories')
+router.register('genres', views.GenreViewSet, basename='genres')
+router.register('titles', views.TitleViewSet, basename='titles')
 router.register(r'titles/(?P<title_id>\d+)/reviews', views.ReviewViewSet, basename='reviews')
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
