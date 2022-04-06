52562fcbfb94c12b8ed26b198a731899d1e26ff8
pozarnik <pozarnik@tut.by>
2022-04-06 06:08:05 +0300

    Update path

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -111,7 +111,7 @@ STATICFILES_DIRS = (BASE_DIR / 'static/',)
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+EMAIL_FILE_PATH = str(BASE_DIR / 'sent_emails')
 
 AUTH_USER_MODEL = 'reviews.User'
 
